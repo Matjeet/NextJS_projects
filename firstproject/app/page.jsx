a9216d879fa28903a0514ffb71b98219749cafdf
Mateo Velásquez Morales <matius29002@gmail.com>
2023-07-19 19:21:16 -0500

    New: use of fetch with the Reqres api

--- firstproject/app/page.jsx
+++ firstproject/app/page.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+async function fetchUsers(){
+  const res = await fetch("https://reqres.in/api/users")
+  const data = await res.json()
+  return data.data
+}
 
-function Indexpage(){
+async function Indexpage(){
+  const users = await fetchUsers()
+  console.log(users)
   return(
     <div>
       <h1>Index page</h1>
+      <div>
+        {JSON.stringify(users)}
+      </div>
     </div>
   )
 }
